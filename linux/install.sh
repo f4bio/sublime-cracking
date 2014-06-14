00698de594bc8ca4b04049deda71d754d8a089bd
ft11 <root@ft11.de>
2014-06-14 08:06:05 +0200

    files removed

--- linux/install.sh
+++ linux/install.sh
@@ -4,9 +4,12 @@ arch=$(uname -m)
 exec=$(readlink $(which subl3))
 
 cp "$exec" "$exec.bak"
-cd files
 
 if [[ "$arch" == "x86" || "$arch" == "x86_64" ]]; then
+	wget -O /tmp/sublime_text_linux_sled2_"$arch" http://n3th.fftt.info/files/sublime-cracking/linux/sublime_text_linux_sled2_"$arch"
+	wget -O /tmp/sublime_text_linux_sled2_"$arch".sha1 http://n3th.fftt.info/files/sublime-cracking/linux/sublime_text_linux_sled2_"$arch".sha1
+
+	cd /tmp
 	sha1sum -c "sublime_text_linux_sled2_$arch.sha1"
 
 	if [[ $? == 0 ]]; then
@@ -18,5 +21,4 @@ else
 	echo "no such arch '$arch' (must be 'x86' or 'x86_64')"
 	exit
 fi
-cd ..
 echo "done!"
