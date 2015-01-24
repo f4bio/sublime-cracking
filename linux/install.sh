3d9e4c2ccb0ba9ef96d8a9f0052b1650b48c3697
Fabio Tea <ft2011@gmail.com>
2015-01-24 08:57:57 +0100

    default autosave

--- linux/install.sh
+++ linux/install.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 
+# setup
 arch=$(uname -m)
 exec=$(readlink $(which subl3))
+fileDownload=$(mktemp)
+fileCrack=$(mktemp)
+fileChecksum=$(mktemp)
 
-cp "$exec" "$exec.bak"
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+echo "-> root: ok"
 
-if [[ "$arch" == "x86" || "$arch" == "x86_64" ]]; then
-	wget -O /tmp/sublime_text_linux_sled2_"$arch" http://n3th.fftt.info/files/sublime-cracking/linux/sublime_text_linux_sled2_"$arch"
-	wget -O /tmp/sublime_text_linux_sled2_"$arch".sha1 http://n3th.fftt.info/files/sublime-cracking/linux/sublime_text_linux_sled2_"$arch".sha1
-
-	cd /tmp
-	sha1sum -c "sublime_text_linux_sled2_$arch.sha1"
-
-	if [[ $? == 0 ]]; then
-		cp "sublime_text_linux_sled2_$arch" "$exec"
-	else
-		echo "file 'sublime_text_linux_sled2_$arch' is corrupt..."
+if [[ "$arch" == "x86_64" || "$arch" == "x86" ]]; then
+	# safty first
+	cp "$exec" "$exec.bak"
+	if [[ $? -ne 0 ]]; then
+		echo "error backing up '$exec'"
+		exit $?
 	fi
-else 
+	echo "-> backup: ok"
+	# download
+	curl -sL --output "$fileDownload" https://db.tt/EH5QQIzK
+	echo "-> download: ok"
+	# unzip
+	unzip -qp "$fileDownload" "sublime_text crack linux 64 build 3065" > "$fileCrack"
+	echo "-> unzip: ok"
+	# check for corruption
+	echo "37a93e50bcdb034d2f40537d225f582c $fileCrack" > "$fileChecksum"
+	md5sum -c "$fileChecksum" > /dev/null
+	if [[ $? -ne 0 ]]; then
+		echo "crack '$fileCrack' is corrupt..."
+		exit $?
+	fi
+	echo "-> checksum: ok"
+	cp "$fileCrack" "$exec"
+	echo "-> copy: ok"
+else
 	echo "no such arch '$arch' (must be 'x86' or 'x86_64')"
 	exit
 fi
-echo "done!"
+echo "--> done!"
